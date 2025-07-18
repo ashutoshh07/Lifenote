340f96d6ea317e7185346f924526b98b65dc113b
Ashutosh <ashutosh.184007@gmail.com>
2025-07-18 07:10:32 +0530

    feat: integrate tailwind & lucide angular icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { Check, Home, LucideAngularModule, PanelLeftClose, PanelRightClose, Repeat2, Settings, Timer } from 'lucide-angular';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    importProvidersFrom(LucideAngularModule.pick({ Home, PanelLeftClose, PanelRightClose, Check, Timer, Repeat2, Settings })),
     provideAnimations(), // For Angular Material animations
     // Add more providers as needed
   ]
